e0cafb96dfca44e4272529e119aacbc73ee6fabe
Dhamodaran Sri <dhamodaran.s@tridentnets.in>
2018-02-10 14:15:06 +0530

    Conflict Commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,6 +8,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
+
+    private func callThisMethod() {
+        
+        print("hello world")
+        print("hello world")
+        newMethod()
+        print("new conflict print")
+        
+    }
+    
+    fileprivate func newMethod() {
+        
+        print("hello world")
+        print("hello world")
+        print("hello world")
+        print("hello world")
+        print("hello world")
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
