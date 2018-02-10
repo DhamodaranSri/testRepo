cad3e96aebbd293e552f0220304d649867575655
Dhamodaran Sri <dhamodaran.s@tridentnets.in>
2018-02-10 13:24:50 +0530

    WIP

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,35 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    private func callThisMethod() {
+        
+        print("hello world")
+        print("hello world")
+        newMethod()
+        
+    }
+    
+    fileprivate func newMethod() {
+        
+        print("hello world")
+        print("hello world")
+        print("hello world")
+        print("hello world")
+        print("hello world")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
